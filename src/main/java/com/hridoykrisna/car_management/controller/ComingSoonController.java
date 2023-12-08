e97e85ce27128ff462ccbd112432c2cd7e0915d0
hridoy <hridoykrisnadas@gmail.com>
2023-12-08 15:54:05 +0600

    Updated 08/12/2023 By Hridoy

--- src/main/java/com/hridoykrisna/car_management/controller/ComingSoonController.java
+++ src/main/java/com/hridoykrisna/car_management/controller/ComingSoonController.java
@@ -1,7 +1,9 @@
 package com.hridoykrisna.car_management.controller;
 
+import com.hridoykrisna.car_management.Utils.CommonUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,5 +13,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @RequestMapping("")
 public class ComingSoonController {
+    @GetMapping("/chat")
+    public String getAIChat(Model model) {
+        if (CommonUtils.isAuthenticate) {
+            model.addAttribute("currentUserName", CommonUtils.employee.getName());
+            return "coming_soon.html";
+        }
+        else {
+            return "redirect:login";
+        }
+    }
+
+    @GetMapping("/car-parking")
+    public String getCarParking(Model model) {
+        if (CommonUtils.isAuthenticate) {
+            model.addAttribute("currentUserName", CommonUtils.employee.getName());
+            return "coming_soon.html";
+        }
+        else {
+            return "redirect:login";
+        }
+    }
+
+    @GetMapping("/car-tracking")
+    public String getCarTracking(Model model) {
+        if (CommonUtils.isAuthenticate) {
+            model.addAttribute("currentUserName", CommonUtils.employee.getName());
+            return "coming_soon.html";
+        }
+        else {
+            return "redirect:login";
+        }
+    }
 
 }
